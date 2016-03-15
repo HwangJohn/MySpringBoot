5376a84bccfce844e148771b4062892c5062c741
HwangJohn <angelic805@gmail.com>
2016-03-15 19:13:30 +0900

    Delete stat0001Module.js

--- src/main/webapp/static/stat0001/modules/stat0001Module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('stat0001Module', [
-        'bsTable','ui.bootstrap','smart-table','angularSpinner', 'nvd3'
-    ])
-    .service('stat0001Service',Stat0001Service)
-    .controller('stat0001Controller', Stat0001Controller);
